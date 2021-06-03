3c46e817cb7763bd0ca9bfe8258c1bd6d51a5fe1
Pawat Saengduan <saengduanpawat@gmail.com>
2021-06-03 15:37:06 +0700

    Fixed the format of code as PEP-8.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,7 +17,6 @@ class AdminSiteTests(TestCase):
             name='Test user full name'
         )
 
-    
     def test_users_listed(self):
         """Test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
@@ -25,7 +24,7 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-    
+
     def test_user_page_change(self):
         """Test that the user edit page works"""
         url = reverse('admin:core_user_change', args=[self.user.id])
@@ -39,5 +38,3 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-
-        
