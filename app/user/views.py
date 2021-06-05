1ae07f9a425a5fed628307bb91b45fa2112282a4
Pawat Saengduan <saengduanpawat@gmail.com>
2021-06-05 09:12:49 +0700

    Fixed typos.

--- app/user/views.py
+++ app/user/views.py
@@ -11,12 +11,12 @@ class CreateUserView(generics.CreateAPIView):
 
 
 class CreateTokenView(ObtainAuthToken):
-    """Create a new auth token for user"""
+    """Create a new auth token for the  user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class ManageUserView(generics.RetrieveAPIView):
+class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user"""
     serializer_class = UserSerializer
     authentication_classes = (authentication.TokenAuthentication,)
